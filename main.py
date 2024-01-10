ab0665fc048bc374e4661f6836cc8c990e2781eb
jelwingeuan <jelwingeuanjw@gmail.com>
2024-01-10 15:20:17 +0800

    10.01-wed-15:20_jelwin-updated

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 1920, 1080
+
+scr = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Animal Run")
+
+#list
+WHITE = (255, 255, 255)
+FPS = 60
+
+
+
+
+
+
+
+
+
+def main():
+#game loop
+    run = True
+    while run:
+        clock.tick  (FPS)#60
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        scr.fill(WHITE)
+        pygame.display.flip()
+
+    #quit game
+    pygame.quit() 
+
+#only to run this "main" func
+if __name__ == "__main__":
+    main()
