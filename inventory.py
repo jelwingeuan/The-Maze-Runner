c7ea1c3aa34101a0a1209399c297abda4279b3a6
yanping-teow <yanpingteow@gmail.com>
2024-01-26 21:59:12 +0800

    inventory

--- inventory.py
+++ inventory.py
@@ -10,12 +10,12 @@ msg = ""
 clear()
 prompt()
 
-#Game loop # stop==win lose or exit
+##Game loop # stop==win lose or exit
 while True:
 
     clear()
 
-    print(f"You are in the {current_section}\n Inventory: {inventory}\n{'-'*27}")
+    print(f"{name}, you are in the {current_section}.\n Inventory: {inventory}\n{'-'*27}")
 
     #Display msg
     print(msg)
@@ -25,17 +25,55 @@ while True:
 
         current_weapon = Section[current_section]["Weapon"]
 
-        if current_weapon not in inventory:
 
-            if current_weapon[-1] == 's':
-                print(f"You see {current_weapon}")
-            
-            elif current_weapon[0] == 'a'or 'e'or'i'or 'o' or'u':
-                print(f"You see an {current_weapon}")
-            
-            else:
-                print(f"You see a {current_weapon}")
 
+        if current_weapon[-1] == 's':
+            print(f"You see {current_weapon}")
+            
+        elif current_weapon[0] == 'a'or 'e'or'i'or 'o' or'u':
+            print(f"You see an {current_weapon}")
+            
+        else:
+            print(f"You see a {current_weapon}")
+
+    class Weapon:
+        def __init__(self, name, power):
+            self.name = name
+            self.power = power
+
+    class Grenades(Weapon):
+        def __init__(self):
+            super().__init__(Grenades, 50)
+    
+    class Bow(Weapon):
+        def __init__(self):
+            super().__init__(Bow, 40)
+    
+    class Sword(Weapon):
+        def __init__(self):
+            super().__init__(Sword,30)
+    
+    class Spear(Weapon):
+        def __init__(self):
+            super().__init__(Spear, 20)
+
+    class Darts(Weapon):
+        def __init__(self):
+            super().__init__(Darts, 10)
+
+    class Player:
+        def __init__(self, name, hp, weapon):
+            self.name = name 
+            self.hp= hp
+            self.weapon = weapon
+
+        def attack(self, monster):
+            self.weapon.attack(monster)
+        
+    class Monster:
+        def __init__(self,name, hp):
+            self.name = name
+            self.hp = hp
 
     # Monster
         
@@ -61,57 +99,32 @@ while True:
         except:
             msg = f"You can't go that way."
 
-    # Picking
+        # Picking
     elif action == "Get":
-            
-        try:    
-            if weapon == Section[current_section]["Weapon"]:
+        try:
+            weapon_to_get = next_move[1].title()
+            current_weapon = Section[current_section]["Weapon"]
 
-                if weapon not in inventory:
-                    inventory.append(Section[current_section]["Weapon"])
-                    msg = f"You got {weapon}!"
-
-                ###
+            if weapon_to_get == current_weapon:
+                if len(inventory)<= 10 :
+                    if weapon_to_get in inventory:
+                        inventory[weapon_to_get] += 1
+                    else:
+                        inventory[weapon_to_get]=1
+                    msg = f"You got {weapon_to_get}! (There are now {inventory[weapon_to_get]} of them in your inventory.)"
                 else:
-                    msg = f"You already have the {weapon}."
-                
+                    msg = "Your inventory is full! "
             else:
-                msg = "You meet Monster!"
-
-        except:
-            msg = "You meet Monster!"
-
+                msg = "There's no such weapon here."
+        except KeyError:
+            msg = "There's no weapon in this section."
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            msg = "Something went wrong."
+            
     #Exit game
     elif action == "Exit":
         break
 
     else:
         msg = "Invalid command"
-
-if {weapon} > 10:
-        print(f"No more space to get {current_weapon}. Is time to attack Monster！ ")
-            
-
-            
-
-
-
-    
-
-
-
-
-
-        
-                
-    
-                
-
-                
-    
-
-            
-
-        
-
-
