90a2f089f633823b8c4cbc7fb847995b72a9442e
jelwingeuan <jelwingeuanjw@gmail.com>
2024-01-27 07:03:07 +0800

    jelwin 7:03am

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import random
 
@@ -15,10 +14,11 @@ def clear():
 
 # Menu
 def prompt():
-
-    print("\t\t\tWelcome To The Maze Runner Game\n\n\
+    print(
+        "\t\t\tWelcome To The Maze Runner Game\n\n\
         Moves:\t'go {direction}'(travel North, South, East, or West)\n\
-        \t'get {item}' (add current weapon to inventory)\n\n")
+        \t'get {item}' (add current weapon to inventory)\n\n"
+    )
 
     input("Press Any Key To Continue...")
 
@@ -38,138 +38,158 @@ Section = {
         "East": "Shadow Labyrinth",
         "South": "The Glade",
         "West": "Grievers' Alley",
-        'Weapon': 'Spear'
+        "Weapon": "Spear",
     },
     "Shadow Labyrinth": {  # 3 Ways
         "North": "Wraith's Walk",
         "East": "Eclipsed Enclave",
         "West": "The Glade Entrance",
-        'Weapon': 'Grenades'
+        "Weapon": "Grenades",
     },
     "Runner' Junction": {  # 2 Ways
         "South": "Enigma Passage",
         "West": "The Glade",
-        'Weapon': 'Sword'
+        "Weapon": "Sword",
     },
     "Enigma Passage": {  # 4 Ways
         "North": "Runners' Junction",
         "East": "Serpent's Spiral",
         "South": "Cryptic Crossroads",
         "West": "Twilight Tunnels",
-        'Monster': 'Grievers'
+        "Monster": "Grievers",
     },
     "Twilight Tunnels": {  # 3 Ways
         "North": "The Glade",
         "East": "Enigma Passage",
         "West": "Solitude Path",
-        'Weapon': 'Bow'
+        "Weapon": "Bow",
     },
     "Solitude Path": {  # 3 Ways
         "North": "Whispering Woods",
         "East": "Twilight Tunnels",
         "West": "Forgotten Nexus",
-        'Weapon': 'Spear'
+        "Weapon": "Spear",
     },
     "Whispering Woods": {  # 4 Ways
         "North": "Grivers' Alley",
         "East": "The Glade",
         "South": "Solitude Path",
         "West": "Nebula Nexus",
-        'Weapon': 'Grenades'
+        "Weapon": "Grenades",
     },
     "Grivers' Alley": {  # 3 Ways
         "North": "Vortex Vestibule",
         "East": "The Glade Entrance",
         "South": "Whispering Woods",
-        'Monster': 'Grievers'
+        "Monster": "Grievers",
     },
     # 3rd Phase of the Maze-----------------------------
     "Echoing Corridor": {  # 3 Ways
         "North": "SECTION 5",
         "East": "Wraith's Walk",
         "South": "The Glade Entrance",
-        'Weapon': 'Bow'
+        "Weapon": "Bow",
     },
     "Wraith's Walk": {  # 3 Ways
         "East": "Abyssal Arches",
         "South": "Shadow Labyrinth",
         "West": "Echoing Corridor",
-        'Monster': 'Grievers'
+        "Monster": "Grievers",
     },
-    "Abyssal Arches": {  # 1 Way
-        "West": "Wraith's Walk",
-        'Weapon': 'Darts'
-    },
+    "Abyssal Arches": {"West": "Wraith's Walk", "Weapon": "Darts"},  # 1 Way
     "Eclipsed Enclave": {  # 3 Ways
         "East": "SECTION 3",
         "South": "Veiled Vista",
         "East": "Shawdow Labyrinth",
-        'Weapon': 'Spear'
+        "Weapon": "Spear",
     },
-    "Veiled Vista": { # 1 Way 
-        "North": "Eclipsed Enclave",
-        'Weapon':'Darts'
-    },
+    "Veiled Vista": {"North": "Eclipsed Enclave", "Weapon": "Darts"},  # 1 Way
     "Serpent's Spiral": {  # 2 Ways
         "South": "Moonlit Maze",
         "East": "Enigma Passage",
-        'Weapon': 'Spear'
+        "Weapon": "Spear",
     },
     "Moonlit Maze": {  # 2 Ways
         "North": "Serpent's Spiral",
         "East": "SECTION 8",
-        'Weapon': 'Darts'
+        "Weapon": "Darts",
     },
     "Cryptic Crossroads": {  # 4 Ways
         "North": "Enigma Passage",
         "South": "SECTION 2",
-        'Weapon': 'Darts'
+        "Weapon": "Darts",
     },
-    "Enchanted Corridor": {  # 1 Way
-        "West": "Starlit Sentries",
-        'Weapon':'Spear'
-    },
+    "Enchanted Corridor": {"West": "Starlit Sentries", "Weapon": "Spear"},  # 1 Way
     "Starlit Sentries": {  # 3 Ways
         "East": "Enchanted Corridor",
         "South": "SECTION 4",
         "West": "Celestial Circuit",
-        'Weapon': 'Sword'
+        "Weapon": "Sword",
     },
     "Celestial Circuit": {  # 2 Ways
         "North": "Forgotten Nexus",
         "East": "Starlit Sentries",
-        'Monster': 'Grievers'
+        "Monster": "Grievers",
     },
     "Forgotten Nexus": {  # 3 Ways
         "East": "Solutide Path",
         "South": "Celestial Circuit",
         "West": "SECTION 6",
-        'Weapon': 'Bow'
+        "Weapon": "Bow",
     },
-    "Nebula Nexus": {  # 1 Way
-        "East": "Whispering Woods",
-        'Monster': 'Grievers'
-    },
+    "Nebula Nexus": {"East": "Whispering Woods", "Monster": "Grievers"},  # 1 Way
     "Mazeheart Nexus": {  # 2 Ways
         "North": "Twilight Traverse",
         "West": "SECTION 7",
-        'Weapon': 'Sword'
+        "Weapon": "Sword",
     },
     "Twilight Traverse": {  # 3 Ways
         "North": "SECTION 1",
         "East": "Vortex Vestibule",
         "South": "Mazeheart Nexus",
-        'Weapon': 'Darts'
+        "Weapon": "Darts",
     },
     "Vortex Vestibule": {  # 2 Ways
         "South": "Grievers' Alley",
         "West": "Twilight Traverse",
-        'Weapon': 'Sword'
+        "Weapon": "Sword",
+    },
+    # Exit Sections--------------------------------
+    "SECTION 1": {
+        "South": "Twilight Traverse",
+    },
+    "SECTION 2": {
+        "Nouth": "Cryptic Crossroads",
+    },
+    "SECTION 3": {
+        "West": "Eclipsed Enclave",
+    },
+    "SECTION 4": {
+        "North": "Starlit Sentries",
+    },
+    "SECTION 5": {
+        "South": "Echoing Corridor",
+    },
+    "SECTION 6": {
+        "East": "Forgotten Nexus",
+    },
+    "SECTION 7": {
+        "East": "Mazeheart Nexus",
+    },
+    "SECTION 8": {
+        "West": "Moonlit Maze",
     },
 }
 
-# User's Inventory max 10
+
+# User's Inventory with a maximum capacity of 10
 inventory = {}
+
+
+# Function to randomize exit sections
+def get_random_exit_section():
+    return random.randint(1, 8)
+
 
 # Track User's Section Location
 current_section = "The Glade"
@@ -178,34 +198,50 @@ current_section = "The Glade"
 msg = ""
 
 
+# Display game menu
 clear()
 prompt()
 
 name = input("Please enter you name: ")
 
-#Game loop # stop==win lose or exit
-while True:
 
+# Game loop # stop==win lose or exit
+while True:
     clear()
 
-    print(f"{name}, you are in the {current_section}.\n Inventory: {inventory}\n{'-'*27}")
+    print(
+        f"{name}, you are in the {current_section}.\n Inventory: {inventory}\n{'-'*27}"
+    )
 
-    #Display msg
+    # Display msg
     print(msg)
 
-    #Weapon
-    if "Weapon" in Section[current_section].keys():
+    # Key Cursor
+    if current_section == "The Glade":
+        correct_exit = random.choice([1, 2, 3, 4, 5, 6, 7, 8])
+        print(f"The correct exit for the key is SECTION {correct_exit}")
 
+    # Check if the player has reached the correct exit section
+    if current_section == f"SECTION {correct_exit}":
+        print("You have reached the correct exit section.")
+
+        # Player activates the key cursor
+        activation_key = input("Press 'a' to activate the key cursor: ").lower()
+        if activation_key == "a":
+            print("Congratulations! You have activated the key cursor. You've escaped the maze")
+            break
+            # Game win condition or any other actions after winning
+
+    # Weapon Indicator
+    if "Weapon" in Section[current_section].keys():
         current_weapon = Section[current_section]["Weapon"]
 
-
-
-        if current_weapon[-1] == 's':
+        if current_weapon[-1] == "s":
             print(f"You see {current_weapon}")
-            
-        elif current_weapon[0] == 'a'or 'e'or'i'or 'o' or'u':
+
+        elif current_weapon[0] == "a" or "e" or "i" or "o" or "u":
             print(f"You see an {current_weapon}")
-            
+
         else:
             print(f"You see a {current_weapon}")
 
@@ -217,15 +253,15 @@ while True:
     class Grenades(Weapon):
         def __init__(self):
             super().__init__(Grenades, 50)
-    
+
     class Bow(Weapon):
         def __init__(self):
             super().__init__(Bow, 40)
-    
+
     class Sword(Weapon):
         def __init__(self):
-            super().__init__(Sword,30)
-    
+            super().__init__(Sword, 30)
+
     class Spear(Weapon):
         def __init__(self):
             super().__init__(Spear, 20)
@@ -234,27 +270,11 @@ while True:
         def __init__(self):
             super().__init__(Darts, 10)
 
-    class Player:
-        def __init__(self, name, hp, weapon):
-            self.name = name 
-            self.hp= hp
-            self.weapon = weapon
-
-        def attack(self, monster):
-            self.weapon.attack(monster)
-        
-    class Monster:
-        def __init__(self,name, hp):
-            self.name = name
-            self.hp = hp
-
-    # Monster
-        
     # For movement
     player_input = input("Enter your move:\n")
 
     # Split move into words
-    next_move = player_input.split(' ')
+    next_move = player_input.split(" ")
 
     # Action first word
     action = next_move[0].title()
@@ -262,8 +282,8 @@ while True:
     if len(next_move) > 1:
         weapon = next_move[1].title()
         direction = next_move[1].title()
-        
-    #Moving
+
+    # Moving
     if action == "Go":
         try:
             current_section = Section[current_section][direction]
@@ -279,11 +299,11 @@ while True:
             current_weapon = Section[current_section]["Weapon"]
 
             if weapon_to_get == current_weapon:
-                if len(inventory)<= 10 :
+                if len(inventory) <= 10:
                     if weapon_to_get in inventory:
                         inventory[weapon_to_get] += 1
                     else:
-                        inventory[weapon_to_get]=1
+                        inventory[weapon_to_get] = 1
                     msg = f"You got {weapon_to_get}! (There are now {inventory[weapon_to_get]} of them in your inventory.)"
                 else:
                     msg = "Your inventory is full! "
@@ -294,12 +314,12 @@ while True:
         except Exception as e:
             print(f"An error occurred: {e}")
             msg = "Something went wrong."
-            
-    #Exit game
+
+    #  #Player VS Monster
+
+    # Exit games
     elif action == "Exit":
         break
 
     else:
         msg = "Invalid command"
-
-            
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,38 @@
-#Clears screen
-# def clear():
-#     os.system('cls' if os.name = 'nt' else 'clear')
+
+            weapon_to_get = next_move[1].title()
+            current_weapon = Section[current_section]["Weapon"]
+
+            if weapon_to_get == current_weapon:
+                if len(inventory) <= 10:
+                    if weapon_to_get in inventory:
+                        inventory[weapon_to_get] += 1
+                    else:
+                        inventory[weapon_to_get] = 1
+                    msg = f"You got {weapon_to_get}! (There are now {inventory[weapon_to_get]} of them in your inventory.)"
+                else:
+                    msg = "Your inventory is full! "
+            else:
+                msg = "There's no such weapon here."
+        except KeyError:
+            msg = "There's no weapon in this section."
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            msg = "Something went wrong."
+
+    #  #Player VS Monster
+
+    # Activate Key and Win
+    elif action == "A":
+        # Check if the player is in the correct exit section and has the key
+        if current_section == f"SECTION {correct_exit}" and "Key" in inventory:
+            print("You have activated the key! You win!")
+            break
+        else:
+            print("You can't activate the key here.")
+
+    # Exit games
+    elif action == "Exit":
+        break
+
+    else:
+        msg = "Invalid command"
